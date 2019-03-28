e5547a278769229937501fe8a2e4f95d047223ea
Kevin Woo <kevinawoo@gmail.com>
2019-03-28 15:18:35 -0700

    add test stub

---
+++ app/scripts/modules/core/src/pipeline/executions/execution/Execution.spec.tsx
@@ -0,0 +1,5 @@
+describe('<Execution/>', () => {
+  // skipped for now because it's a bit hard to test these and will be handled later
+  xit('adds parameters, sorted alphabetically');
+  xit('excludes some parameters if the pipeline is a strategy');
+});
