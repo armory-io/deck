127d0aa37c5c546afe14a910471cb7215baf6243
Brandon Powell <brandon.powell@armory.io>
2019-05-12 17:28:17 -0700

    feat(runjob): start of property or artifact based logs

--- app/scripts/modules/kubernetes/src/v2/pipelines/stages/runJob/KubernetesV2RunJobStageConfig.tsx
+++ app/scripts/modules/kubernetes/src/v2/pipelines/stages/runJob/KubernetesV2RunJobStageConfig.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
 
-import { IStageConfigProps, AccountService, YamlEditor, yamlDocumentsToString, IAccount } from '@spinnaker/core';
+import {
+  SETTINGS,
+  ExpectedArtifactSelector,
+  StageArtifactSelector,
+  StageConfigField,
+  IStageConfigProps,
+  AccountService,
+  YamlEditor,
+  yamlDocumentsToString,
+  IAccount,
+} from '@spinnaker/core';
 
 import { ManifestBasicSettings } from 'kubernetes/v2/manifest/wizard/BasicSettings';
+import Select from 'react-select';
 
 export interface IKubernetesRunJobStageConfigState {
   credentials: IAccount[];
@@ -12,6 +23,13 @@ export interface IKubernetesRunJobStageConfigState {
 export class KubernetesV2RunJobStageConfig extends React.Component<IStageConfigProps> {
   public state: IKubernetesRunJobStageConfigState = {
     credentials: [],
+    options: [
+      { value: 'none', label: 'None' },
+      { value: 'logs', label: 'Logs' },
+      { value: 'artifacts', label: 'Artifacts' },
+    ],
+    selected: { value: 'none', label: 'None' },
+    selectedArtifact: {},
   };
 
   public accountChanged = (account: string) => {
@@ -43,6 +61,19 @@ export class KubernetesV2RunJobStageConfig extends React.Component<IStageConfigP
     this.initRawManifest();
   }
 
+  private handleOutputTypeChange = (selected: any) => {
+    this.setState({ selected: selected });
+  };
+
+  private handlePropertyFile = (e: any) => {
+    this.props.updateStageField({ propertyFile: e.target.value });
+  };
+
+  private stuff = (e: any) => {
+    this.props.updateStageField({ consumeArtifactID: e.id });
+    this.setState({ selectedArtifact: e });
+  };
+
   public render() {
     const { application, stage } = this.props;
 
@@ -55,6 +86,46 @@ export class KubernetesV2RunJobStageConfig extends React.Component<IStageConfigP
           accounts={this.state.credentials}
           onAccountSelect={(selectedAccount: string) => this.accountChanged(selectedAccount)}
         />
+        <h4>Job Output</h4>
+        <StageConfigField helpKey="kubernetes.manifest.delete.gracePeriod" label="Type">
+          <Select
+            className="form-control input-sm"
+            onChange={this.handleOutputTypeChange}
+            options={this.state.options}
+            value={this.state.selected}
+            clearable={false}
+            required={true}
+          />
+          {this.state.selected.value === 'logs' && (
+            <StageConfigField label="Property File">
+              <input onChange={this.handlePropertyFile} type="text" />
+            </StageConfigField>
+          )}
+          {this.state.selected.value === 'artifacts' && SETTINGS.feature['artifactsRewrite'] && (
+            <StageArtifactSelector
+              pipeline={this.props.pipeline}
+              stage={this.props.stage}
+              // expectedArtifactId={manifest && manifest.artifactId}
+              // artifact={manifest && manifest.artifact}
+              onExpectedArtifactSelected={this.stuff}
+              onArtifactEdited={this.stuff}
+              // excludedArtifactTypePatterns={this.excludedArtifactTypePatterns}
+              // renderLabel={(field: React.ReactNode) => {
+              //   return <FormikConfigField label={'Artifact'}>{field}</FormikConfigField>;
+              // }}
+            />
+          )}
+          {this.state.selected.value === 'artifacts' && !SETTINGS.feature['artifactsRewrite'] && (
+            <ExpectedArtifactSelector
+              // excludedArtifactTypes={excludedArtifactTypes}
+              expectedArtifacts={this.props.pipeline.expectedArtifacts}
+              // selected={this.getSelectedExpectedArtifact(selectedArtifactId)}
+              onChange={this.stuff}
+              // onRequestCreate={this.onRequestCreateArtifact}
+              // requestingNew={showCreateArtifactForm}
+            />
+          )}
+        </StageConfigField>
         <h4>Manifest Configuration</h4>
         <YamlEditor value={this.state.rawManifest} onChange={this.handleRawManifestChange} />
       </div>
